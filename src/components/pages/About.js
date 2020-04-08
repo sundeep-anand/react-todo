3a979833e6548c90383f8346d151f262341ad884
Sundeep Anand <suanand@redhat.com>
2020-04-08 17:36:13 +0530

    react todo app

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>
+                This is the TodoList app v0.1.0<br/>
+                It is a part of a React crash course.
+            </p>
+        </React.Fragment>
+    )
+}
+
+export default About;
